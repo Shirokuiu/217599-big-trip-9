b4fb9cc7046bb8935a81e06402f0b37cb9b6c69b
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2019-08-25 19:57:25 +0300

    Завершил module5-task2

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -3,24 +3,33 @@ import TripDay from "../components/trip-day";
 import Sort from "../components/sort";
 import Point from "../components/point";
 import PointEdit from "../components/point-edit";
+import NoEventsScreen from "../components/noEventsScreen";
 
-import {render, APP_SETTINGS} from '../utils';
+import {render} from '../utils';
 
 export default class TripController {
-  constructor(container, pointMocks) {
+  constructor(container, pointMocks, appInfo) {
     this._container = container;
     this._pointMocks = pointMocks;
+    this._appInfo = appInfo;
     this._sort = new Sort();
     this._content = new Content();
     this._tripDay = new TripDay();
+    this._noEventsScreen = new NoEventsScreen();
   }
 
   init() {
-    render(this._container, this._sort.getElement());
-    render(this._container, this._content.getElement());
-    render(this._content.getElement(), this._tripDay.getElement());
+    if (this._appInfo.totalPoints) {
+      render(this._container, this._sort.getElement());
+      render(this._container, this._content.getElement());
+      render(this._content.getElement(), this._tripDay.getElement());
+      this._pointMocks.forEach((point) => this._renderPoint(point, this._appInfo));
 
-    this._pointMocks.forEach((point) => this._renderPoint(point, APP_SETTINGS));
+      this._sort.getElement().querySelector(`.trip-events__trip-sort`)
+        .addEventListener(`change`, this._sortPointMocks.bind(this));
+      return;
+    }
+    render(this._container, this._noEventsScreen.getElement());
   }
 
   _renderPoint(pointMocks, settings) {
@@ -50,5 +59,28 @@ export default class TripController {
     });
 
     render(document.querySelector(`.trip-events__list`), point.getElement());
+  }
+
+  _sortPointMocks(evt) {
+    this._tripDay.getElement().querySelector(`.trip-events__list`).innerHTML = ``;
+    switch (evt.target.id) {
+      case `sort-event`:
+        this._pointMocks.forEach((point) => this._renderPoint(point, this._appInfo));
+        break;
+      case `sort-time`:
+        this._copyMocks(this._pointMocks)
+          .sort((a, b) => a.dates[0].timeDifference - b.dates[0].timeDifference)
+          .forEach((point) => this._renderPoint(point, this._appInfo));
+        break;
+      case `sort-price`:
+        this._copyMocks(this._pointMocks)
+          .sort((a, b) => a.prices[0] - b.prices[0])
+          .forEach((point) => this._renderPoint(point, this._appInfo));
+        break;
+    }
+  }
+
+  _copyMocks(pointMocks) {
+    return pointMocks.slice();
   }
 }
